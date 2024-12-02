b86636fa47067a8bfcd3251f81e8db527f7294f2
YLudvig <yslu24ne@student.ju.se>
2024-12-02 19:16:46 +0100

    start

---
+++ NBA-test/src/main/java/com/NBAapp/NBA_test/NbaTestApplication.java
@@ -0,0 +1,13 @@
+package com.NBAapp.NBA_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbaTestApplication.class, args);
+	}
+
+}
---
+++ NBA-test/src/test/java/com/NBAapp/NBA_test/NbaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NBAapp.NBA_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
